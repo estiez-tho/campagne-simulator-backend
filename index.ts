ea6a656f20ea237ea1c13ad135cc56c9fc457cf9
thet4 <thomas.estiez@gmail.com>
2020-05-17 15:40:07 +0800

    fix : fixed port message

--- index.ts
+++ index.ts
@@ -21,5 +21,5 @@ mongoose
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log("App is listening on port 3000!");
+  console.log(`App is listening on port ${port}`);
 });
