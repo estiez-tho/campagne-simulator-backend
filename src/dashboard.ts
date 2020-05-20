237bcec61a1371b06a0064be05ca821399af1d99
thet4 <thomas.estiez@gmail.com>
2020-05-20 22:49:10 +0800

    fix: fixed order

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -3,6 +3,6 @@ import { UserInfoModel } from "./user/model";
 export async function getRankedPlayers(): Promise<Array<string>> {
   const playerList = await UserInfoModel.find()
     .select({ username: 1, amount: 1, _id: 0 })
-    .sort({ amount: 1 });
+    .sort({ amount: -1 });
   return playerList;
 }
