f1622f7ffbb20e22e86a743e3b0580c1d8b02685
thet4 <thomas.estiez@gmail.com>
2020-05-20 22:48:44 +0800

    fix: removed id

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -2,7 +2,7 @@ import { UserInfoModel } from "./user/model";
 
 export async function getRankedPlayers(): Promise<Array<string>> {
   const playerList = await UserInfoModel.find()
-    .select({ username: 1, amount: 1 })
+    .select({ username: 1, amount: 1, _id: 0 })
     .sort({ amount: 1 });
   return playerList;
 }
