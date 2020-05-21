34b0188e6d7143018824523afaa1aa8af39180b7
thet4 <thomas.estiez@gmail.com>
2020-05-21 14:57:11 +0800

    added debug

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -18,7 +18,7 @@ function updateScore(userInfo: UserInfo) {
   let deltaAmount = 0;
   const items = { ...userInfo.items };
   const itemsKeys = Object.keys(items).filter((elem) => "01234".includes(elem));
-  console.log(itemsKeys);
+  console.log(Object.keys(items));
   itemsKeys.forEach((id: string) => {
     console.log(items[id]);
     let {
