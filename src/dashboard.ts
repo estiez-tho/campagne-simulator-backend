7d3e28a5af81d2e3b9e0d06309c0c2105df607f9
thet4 <thomas.estiez@gmail.com>
2020-05-21 15:20:06 +0800

    added debug

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -3,20 +3,25 @@ import { UserInfoModel, UserInfo } from "./user/model";
 export async function getRankedPlayers(): Promise<
   Array<{ username: string; amount: number }>
 > {
-  const playerList: Array<UserInfo> = await UserInfoModel.find({}, { _id: 0 });
-  let mapped = playerList.map((elem) => updateScore(elem));
-
-  mapped.sort((left, right) => {
-    return right.amount > left.amount ? 1 : -1;
+  const playerList: Array<UserInfo> = await new Promise((resolve, reject) => {
+    UserInfoModel.find({}, { _id: 0 }).exec((err, res) => {
+      if (err) reject(err);
+      let mapped = res.map((elem) => updateScore(elem));
+      mapped.sort((left, right) => {
+        return right.amount > left.amount ? 1 : -1;
+      });
+      resolve(mapped);
+    });
   });
-  return mapped;
+
+  return playerList;
 }
 
 function updateScore(userInfo: UserInfo) {
   let { username, amount } = userInfo;
   let deltaAmount = 0;
   const items = { ...userInfo.items };
-  const itemsKeys = Object.keys(items).filter((elem) => "01234".includes(elem));
+  const itemsKeys = Object.keys(items);
   console.log(JSON.stringify(items));
   itemsKeys.forEach((id: string) => {
     console.log(items[id]);
